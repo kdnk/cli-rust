da2e3c19b0767427508f27ab9ede661628a8965f
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2024-02-19 22:32:09 +0900

    chore(wcr): show args

---
+++ wcr/src/lib.rs
@@ -0,0 +1,84 @@
+use clap::{App, Arg};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: bool,
+    words: bool,
+    bytes: bool,
+    chars: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("wcr")
+        .version("0.1.0")
+        .author("Kodai Nakamura")
+        .about("wc")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .default_value("-")
+                .multiple(true),
+        )
+        .arg( Arg::with_name("words") .short("w")
+                .long("words")
+                .value_name("WORDS")
+                .takes_value(false)
+                .help("Show word count"),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .value_name("BYTES")
+                .takes_value(false)
+                .help("Show byte count"),
+        )
+        .arg(
+            Arg::with_name("chars")
+                .short("m")
+                .long("chars")
+                .value_name("CHARS")
+                .help("Show character count")
+                .takes_value(false)
+                .conflicts_with("bytes"),
+        )
+        .arg(
+            Arg::with_name("lines")
+                .short("l")
+                .long("lines")
+                .value_name("LINES")
+                .takes_value(false)
+                .help("Show line count"),
+        )
+        .get_matches();
+
+    let mut lines = matches.is_present("lines");
+    let mut words = matches.is_present("words");
+    let mut bytes = matches.is_present("bytes");
+    let mut chars = matches.is_present("chars");
+
+    if !lines && !words && !bytes && !chars {
+        lines = true;
+        words = true;
+        bytes = true;
+        chars = false;
+    }
+
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        lines,
+        words,
+        bytes,
+        chars,
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+    Ok(())
+}
