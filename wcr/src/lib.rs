fd8e1a1c7db875a6b2146107548906bebaea774e
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2024-03-04 23:19:25 +0900

    chore(wcr): pass tests except test_all

--- wcr/src/lib.rs
+++ wcr/src/lib.rs
@@ -1,5 +1,9 @@
 use clap::{App, Arg};
-use std::error::Error;
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -10,6 +14,14 @@ pub struct Config {
     words: bool,
     bytes: bool,
     chars: bool,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct FileInfo {
+    num_lines: usize,
+    num_words: usize,
+    num_bytes: usize,
+    num_chars: usize,
 }
 
 pub fn get_args() -> MyResult<Config> {
@@ -24,7 +36,9 @@ pub fn get_args() -> MyResult<Config> {
                 .default_value("-")
                 .multiple(true),
         )
-        .arg( Arg::with_name("words") .short("w")
+        .arg(
+            Arg::with_name("words")
+                .short("w")
                 .long("words")
                 .value_name("WORDS")
                 .takes_value(false)
@@ -62,7 +76,7 @@ pub fn get_args() -> MyResult<Config> {
     let mut bytes = matches.is_present("bytes");
     let mut chars = matches.is_present("chars");
 
-    if !lines && !words && !bytes && !chars {
+    if [lines, words, bytes, chars].iter().all(|v| v == &false) {
         lines = true;
         words = true;
         bytes = true;
@@ -79,6 +93,88 @@ pub fn get_args() -> MyResult<Config> {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:#?}", config);
+    for filename in config.files {
+        match open(&filename) {
+            Err(err) => eprintln!("{}: {}", &filename, err),
+            Ok(file) => {
+                let info = count(file)?;
+
+                if config.lines {
+                    print!("{:>8}", info.num_lines)
+                }
+
+                if config.words {
+                    print!("{:>8}", info.num_words)
+                }
+
+                if config.chars {
+                    print!("{:>8}", info.num_chars);
+                } else if config.bytes {
+                    print!("{:>8}", info.num_bytes);
+                }
+
+                if &filename != "-" {
+                    println!(" {}", &filename);
+                } else {
+                    println!();
+                }
+            }
+        }
+    }
     Ok(())
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+pub fn count(mut file: impl BufRead) -> MyResult<FileInfo> {
+    let mut num_lines = 0;
+    let mut num_words = 0;
+    let mut num_chars = 0;
+    let mut num_bytes = 0;
+    let mut line = String::new();
+
+    loop {
+        let line_bytes = file.read_line(&mut line)?;
+        if line_bytes == 0 {
+            break;
+        }
+
+        num_bytes += line_bytes;
+        num_lines += 1;
+        num_words += line.split_whitespace().count();
+        num_chars += line.chars().count();
+        line.clear();
+    }
+
+    Ok(FileInfo {
+        num_lines,
+        num_words,
+        num_bytes,
+        num_chars,
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{count, FileInfo};
+    use std::io::Cursor;
+
+    #[test]
+    fn test_count() {
+        let text = "I don't want the world. I just want your half.\r\n";
+        let info = count(Cursor::new(text));
+        assert!(info.is_ok());
+        let expected = FileInfo {
+            num_lines: 1,
+            num_words: 10,
+            num_chars: 48,
+            num_bytes: 48,
+        };
+        assert_eq!(info.unwrap(), expected)
+    }
 }
